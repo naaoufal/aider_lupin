c30ef09ad97936eabf01654751302adf8f2b26f9
naaoufal <naoufelbenmensour@gmail.com>
2021-03-12 17:19:26 +0100

    push files

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const socket = require("socket.io")
+const cors = require('cors');
+require("dotenv").config()
+const connectDB = require("./back_end/config/mongodb")
+const app = express()
+
+connectDB()
+
+app.use(express.json());
+
+app.use(cors());
+
+// declare our endpoints here:
+
+
+
+// start the server
+var port = process.env.PORT || 3000
+var server = app.listen(port, () => console.log("the server is started"));
+var io = socket(server)
