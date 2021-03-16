48c19edaf90c9a49a28c53c86a826d67d93e0da7
naaoufal <naoufelbenmensour@gmail.com>
2021-03-16 10:09:00 +0100

    add files

---
+++ back_end/midllewares/adminauth.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const Admins = require('../models/admins')
+
+module.exports = function auth (req, res, next) {
+    const autHeader = req.headers['authorization']
+    const token = autHeader && autHeader.split(' ')[1]
+  
+    if(token == null){
+        return res.sendStatus(403)
+    }
+  
+    const code = jwt.verify(token, process.env.ACCESS_TOKEN_NORMAL)
+    const admin = Admins.findById(code.id)
+
+    if(!admin){
+        return res.sendStatus(404)
+    }
+    req.admin = admin
+    next()
+}
