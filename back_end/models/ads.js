101b67e328c07822867e706cbf2091ffbb90ccfe
naaoufal <naoufelbenmensour@gmail.com>
2021-04-02 13:33:32 +0100

    add files

---
+++ back_end/models/ads.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const adsSchema = new mongoose.Schema({
+
+    image : {
+        type : String,
+        //required : true,
+    },
+    price : {
+        type : Number,
+        //required : true,
+    },
+    desc : {
+        type : Number,
+        //required : true
+    }
+
+})
+
+module.exports = mongoose.model('ads', adsSchema)
---
+++ back_end/routes/ads.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../lupin/front_end/public/images')
+    },
+    filename : function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+})
+
+const upload = multer({storage : storage})
+
+// declare our routes
+const adsCon = require("../controllers/ads")
+
+
+
+router.post("/add", upload.single('image'), adsCon.add)
+
+router.get("/all", productCon.all)
+
+router.delete("/delete/:id", adsCon.deleteOne)
+
+
+module.exports = router
