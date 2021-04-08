5eb498c2fd78eb4a285a108d084e31376fa3899a
naaoufal <naoufelbenmensour@gmail.com>
2021-04-08 10:08:13 +0100

    add files

---
+++ front_end/src/components/fireSale/firesale.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+    // config
+})
+
+function FireSale () {
+    return ()
+}
+
+export default FireSale
